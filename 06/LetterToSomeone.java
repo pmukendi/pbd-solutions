aa5552d2d3faef67c810604402c44e6375fdd5f2
phil <36393437+pmukendi@users.noreply.github.com>
2018-02-27 10:22:26 -0500

    Add files via upload

---
+++ 06/LetterToSomeone.java
@@ -0,0 +1,15 @@
+public class LetterToSomeone {
+    public static void main(String[] args) {
+        System.out.println("+---------------------------------------------------------+");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                    #### |");
+        System.out.println("|                                                         |");
+        System.out.println("|                                                         |");
+        System.out.println("|                              Jimmbo bobo                |");
+        System.out.println("|                              213 Gucci HQ               |");
+        System.out.println("|                              Edmund, WA 555555          |");
+        System.out.println("|                                                         |");
+        System.out.println("+---------------------------------------------------------+");
+    }
+}
---
+++ 07/MyInitials.java
@@ -0,0 +1,7 @@
+public class MyInitials {
+    public static void main(String[] args) {
+        System.out.println("PPPP   M   M");
+        System.out.println("P   P  MM MM");
+        System.out.println("P   P  MM MM");
+        System.out.println("PPPP   M M  ");
+        System.out.println("P     
